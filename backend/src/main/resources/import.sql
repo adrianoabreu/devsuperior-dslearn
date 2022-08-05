2ccbf110ac88d4dbbfc5a9f3a348d884d76fa325
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-08-05 12:43:16 -0300

    Course

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course(name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML','https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg','http://fabrica.ms.senac.br/wp-content/uploads/2015/06/Como-adicionar-fontes-diferentes-no-HTML-1.jpg');
